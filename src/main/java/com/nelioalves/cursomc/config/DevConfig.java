0c2772b74c9f7d33d21a337598e77669258032dc
Jeterson Miranda <jetersonsi@gmail.com>
2018-05-04 15:51:20 -0400

    implementando o SmtpEmailService

--- src/main/java/com/nelioalves/cursomc/config/DevConfig.java
+++ src/main/java/com/nelioalves/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.nelioalves.cursomc.services.DBService;
+import com.nelioalves.cursomc.services.EmailService;
+import com.nelioalves.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -27,5 +29,10 @@ public class DevConfig {
 
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService smtpEmailService() {
+		return new SmtpEmailService();
 	}
 }
